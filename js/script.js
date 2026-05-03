57ffca0244d8abf1cf9e8b430bd2760434c0500f
ValeriiJavalesson <126103594+ValeriiJavalesson@users.noreply.github.com>
2026-05-03 21:18:23 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-// Перевірка і створення змінних, якщо вони чомусь не визначені
-if (typeof isTrackingActive === 'undefined') window.isTrackingActive = false;
-if (typeof followModeActive === 'undefined') window.followModeActive = false;
-
 let watchId = null;
 let myLocationMarker = null;
 let lastLocation = null;
