28e06e15b0459c3b4ebe1ff6b13d254b3cd5c0a2
ValeriiJavalesson <126103594+ValeriiJavalesson@users.noreply.github.com>
2026-06-05 01:10:35 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting()) // Активує SW відразу
+      .then(() => self.skipWaiting())
   );
 });
 self.addEventListener('activate', (e) => {
